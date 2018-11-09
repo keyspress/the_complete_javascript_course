9f69aa997ecd81d0fe81416ac15f1804b8ea5b40
Kyle Desktop <kylespresser@gmail.com>
2018-11-09 13:45:18 -0500

    lecture 8

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,16 @@
-console.log('What is up world?');
+var firstName = 'Johnny';
+console.log(firstName);
+
+var lastName = 'Jones';
+var age = 99;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'teacher';
+console.log(job);
+
+// var 3years = 3;
