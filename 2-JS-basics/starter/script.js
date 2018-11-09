0dde338fd909b0557488c8b1e70e3541ecae4e49
Kyle Desktop <kylespresser@gmail.com>
2018-11-09 13:57:02 -0500

    lecture 9

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,16 +1,43 @@
-var firstName = 'Johnny';
-console.log(firstName);
+/*****************************
+ *   Variables and data types
+ */
+// var firstName = 'Johnny';
+// console.log(firstName);
 
-var lastName = 'Jones';
-var age = 99;
+// var lastName = 'Jones';
+// var age = 99;
 
-var fullAge = true;
-console.log(fullAge);
+// var fullAge = true;
+// console.log(fullAge);
 
-var job;
-console.log(job);
+// var job;
+// console.log(job);
 
-job = 'teacher';
-console.log(job);
+// job = 'teacher';
+// console.log(job);
 
 // var 3years = 3;
+
+/*****************************
+ *   Variable mutation and type coercion
+ */
+
+var firstName = 'Jim';
+var age = 99;
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = true;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + ' ' + isMarried);
+console.log(age + isMarried);
+
+// variable mutation
+age = 'ninety nine';
+job = 'retired';
+
+// alert(age);
+var lastName = prompt('what is his last name?');
+console.log(lastName);
