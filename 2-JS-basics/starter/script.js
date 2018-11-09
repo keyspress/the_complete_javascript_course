a908362fdbf9b2858b478fce8f0244c1b8656bac
Kyle Desktop <kylespresser@gmail.com>
2018-11-09 15:18:33 -0500

    lecture 18

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -54,14 +54,81 @@
 // console.log(typeof year);
 // console.log(typeof NaN);
 
-var markMass = 50;
-var johnMass = 60;
-var markHeight = 10;
-var johnHeight = 8;
+// var markMass = 50;
+// var johnMass = 60;
+// var markHeight = 10;
+// var johnHeight = 8;
 
-var markBMI = markMass / (markHeight * markHeight);
-var johnBMI = johnMass / (johnHeight * johnHeight);
+// var markBMI = markMass / (markHeight * markHeight);
+// var johnBMI = johnMass / (johnHeight * johnHeight);
 
-var markHigher = markBMI > johnBMI;
+// var markHigher = markBMI > johnBMI;
 
-console.log(`Is Mark's BMI higher than John's? ${markHigher}`);
+// console.log(`Is Mark's BMI higher than John's? ${markHigher}`);
+
+/*****************************
+ *   The Ternary Operator and Switch Statements
+ */
+
+// var firstName = 'John';
+// var age = 16;
+
+// age >= 18 ? console.log('drink up') : console.log('watch out for the po po!');
+
+// var drink = age >= 18 ? 'beer' : 'milk I guess...';
+
+// console.log(`${firstName} can drink ${drink}`);
+
+// var job = 'musician';
+
+// switch (job) {
+//   case 'bar tender':
+//     console.log(`${firstName} listens to drunk people complain`);
+//     break;
+//   case 'teacher':
+//     console.log(
+//       `${firstName} has to deal with bratty idiots and sometimes teach their children`
+//     );
+//     break;
+//   default:
+//     console.log(`${firstName} is basically a bum`);
+// }
+
+/*****************************
+ *   Truthy and Falsy
+ */
+// falsy: undefined, null, 0, '', NaN
+
+// var height;
+// if (height) {
+//   console.log('variable is defined');
+// } else {
+//   console.log('not defined');
+// }
+
+/*****************************
+ *   CODE CHALLENGE 2
+ */
+
+johnTeamScores = [89, 120, 103];
+mikeTeamScores = [116, 9499, 123];
+maryTeamScores = [97, 134, 105];
+
+const calcAverage = scores => {
+  const sum = scores.reduce((a, b) => a + b, 0);
+  return sum / scores.length;
+};
+
+johnTeamAvg = calcAverage(johnTeamScores);
+mikeTeamAvg = calcAverage(mikeTeamScores);
+maryTeamAvg = calcAverage(maryTeamScores);
+
+if (johnTeamAvg > mikeTeamAvg && johnTeamAvg > maryTeamAvg) {
+  console.log('John has the best team');
+} else if (mikeTeamAvg > johnTeamAvg && mikeTeamAvg > maryTeamAvg) {
+  console.log('Mike has the best team');
+} else if (maryTeamAvg > johnTeamAvg && maryTeamAvg > mikeTeamAvg) {
+  console.log('Mary has the best team');
+} else {
+  console.log('No team is best so they are all losers...');
+}
