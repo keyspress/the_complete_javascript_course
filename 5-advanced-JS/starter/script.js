1acafa67bd8a2c1ee435073a30afd92ab1197b8d
Kyle Desktop <kylespresser@gmail.com>
2018-11-14 14:17:32 -0500

    lecture 65

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -49,36 +49,71 @@
 
 // ************************** primitives vs objects ***************************************************
 
-var a = 23;
-var b = a;
+// var a = 23;
+// var b = a;
 
-a = 46;
-console.log(a, b);
+// a = 46;
+// console.log(a, b);
 
-var obj1 = {
-  name: 'john',
-  age: 26
-};
+// var obj1 = {
+//   name: 'john',
+//   age: 26
+// };
 
-var obj2 = obj1;
+// var obj2 = obj1;
 
-obj1.age = 99;
+// obj1.age = 99;
 
-console.log(obj1.age);
-console.log(obj2.age);
+// console.log(obj1.age);
+// console.log(obj2.age);
 
-var age = 24;
-var obj = {
-  name: 'Jim Bob',
-  city: 'Whoville'
-};
+// var age = 24;
+// var obj = {
+//   name: 'Jim Bob',
+//   city: 'Whoville'
+// };
 
-function change(a, b) {
-  a = 30;
-  b.city = 'Atlantis';
+// function change(a, b) {
+//   a = 30;
+//   b.city = 'Atlantis';
+// }
+
+// change(age, obj);
+
+// console.log(age);
+// console.log(obj.city);
+
+// *************************** functions ***********************************************************
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
 }
 
-change(age, obj);
+function calculateAge(el) {
+  return 2018 - el;
+}
 
-console.log(age);
-console.log(obj.city);
+function isFullAge(el) {
+  return el >= 18;
+}
+
+function maxHeartRate(el) {
+  if (el >= 18 && el <= 81) {
+    return Math.round(206.9 - 0.67 * el);
+  } else {
+    return -1;
+  }
+}
+
+var ages = arrayCalc(years, calculateAge);
+console.log(ages);
+var fullAges = arrayCalc(ages, isFullAge);
+console.log(fullAges);
+var rates = arrayCalc(ages, maxHeartRate);
+console.log(rates);
