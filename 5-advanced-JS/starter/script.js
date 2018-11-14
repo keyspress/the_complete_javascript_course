f5ba939928f88c5041bfdae1cb600ffa323ccbf4
Kyle Desktop <kylespresser@gmail.com>
2018-11-14 14:27:01 -0500

    lecture 66

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -85,35 +85,58 @@
 
 // *************************** functions ***********************************************************
 
-var years = [1990, 1965, 1937, 2005, 1998];
+// var years = [1990, 1965, 1937, 2005, 1998];
 
-function arrayCalc(arr, fn) {
-  var arrRes = [];
-  for (var i = 0; i < arr.length; i++) {
-    arrRes.push(fn(arr[i]));
-  }
-  return arrRes;
-}
+// function arrayCalc(arr, fn) {
+//   var arrRes = [];
+//   for (var i = 0; i < arr.length; i++) {
+//     arrRes.push(fn(arr[i]));
+//   }
+//   return arrRes;
+// }
 
-function calculateAge(el) {
-  return 2018 - el;
-}
+// function calculateAge(el) {
+//   return 2018 - el;
+// }
 
-function isFullAge(el) {
-  return el >= 18;
-}
+// function isFullAge(el) {
+//   return el >= 18;
+// }
 
-function maxHeartRate(el) {
-  if (el >= 18 && el <= 81) {
-    return Math.round(206.9 - 0.67 * el);
+// function maxHeartRate(el) {
+//   if (el >= 18 && el <= 81) {
+//     return Math.round(206.9 - 0.67 * el);
+//   } else {
+//     return -1;
+//   }
+// }
+
+// var ages = arrayCalc(years, calculateAge);
+// console.log(ages);
+// var fullAges = arrayCalc(ages, isFullAge);
+// console.log(fullAges);
+// var rates = arrayCalc(ages, maxHeartRate);
+// console.log(rates);
+
+function interviewQuestion(job) {
+  if (job === 'designer') {
+    return function(name) {
+      console.log(name + ', can you please explain what the color red is?');
+    };
+  } else if (job === 'teacher') {
+    return function(name) {
+      console.log(name + ', why are teenagers such idiots?');
+    };
   } else {
-    return -1;
+    return function(name) {
+      console.log(name + ', why are you such a lazy bum?');
+    };
   }
 }
 
-var ages = arrayCalc(years, calculateAge);
-console.log(ages);
-var fullAges = arrayCalc(ages, isFullAge);
-console.log(fullAges);
-var rates = arrayCalc(ages, maxHeartRate);
-console.log(rates);
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+
+teacherQuestion('Elmo');
+designerQuestion('Bilbo');
+interviewQuestion('eater')('Wilbur');
