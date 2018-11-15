7f90e13fbd2c72363d97aaa3603090295a4b5b87
Kyle Desktop <kylespresser@gmail.com>
2018-11-15 13:00:59 -0500

    lecture 86

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -17,6 +17,14 @@ var budgetController = (function() {
     this.value = value;
   };
 
+  var calculateTotal = function(type) {
+    var sum = 0;
+    data.allItems[type].forEach(function(cur) {
+      sum += cur.value;
+    });
+    data.totals[type] = sum;
+  };
+
   var data = {
     allItems: {
       exp: [],
@@ -25,7 +33,9 @@ var budgetController = (function() {
     totals: {
       exp: 0,
       inc: 0
-    }
+    },
+    budget: 0,
+    percentage: -1
   };
 
   return {
@@ -47,6 +57,24 @@ var budgetController = (function() {
       data.allItems[type].push(newItem);
 
       return newItem;
+    },
+    calculateBudget: function() {
+      calculateTotal('exp');
+      calculateTotal('inc');
+      data.budget = data.totals.inc - data.totals.exp;
+      if (data.totals.inc > 0) {
+        data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+      } else {
+        data.percentage = -1;
+      }
+    },
+    getBudget: function() {
+      return {
+        budget: data.budget,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+        percentage: data.percentage
+      };
     },
     testing: function() {
       console.log(data);
@@ -140,8 +168,11 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   var updateBudget = function() {
     // 1. calculate budget
+    budgetCtrl.calculateBudget();
     // 2. return budget
+    var budget = budgetCtrl.getBudget();
     // 3. display budget on ui
+    console.log(budget);
   };
 
   var ctrlAddItem = function() {
