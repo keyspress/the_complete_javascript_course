c8bcf87b281773a4c8e61c7695e9f0b1f2f23d69
Kyle Desktop <kylespresser@gmail.com>
2018-11-16 11:28:17 -0500

    lecture 91

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -58,6 +58,16 @@ var budgetController = (function() {
 
       return newItem;
     },
+    deleteItem: function(type, id) {
+      var ids, index;
+      ids = data.allItems[type].map(function(current) {
+        return current.id;
+      });
+      index = ids.indexOf(id);
+      if (index !== -1) {
+        data.allItems[type].splice(index, 1);
+      }
+    },
     calculateBudget: function() {
       calculateTotal('exp');
       calculateTotal('inc');
@@ -226,8 +236,9 @@ var controller = (function(budgetCtrl, UICtrl) {
     if (itemID) {
       splitID = itemID.split('-');
       type = splitID[0];
-      ID = splitID[1];
+      ID = parseInt(splitID[1]);
       // 1. delete item from data
+      budgetCtrl.deleteItem(type, ID);
       // 2. delete item from ui
       // 3. update and show the new budget
     }
