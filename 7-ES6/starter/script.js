1524f0f88c51507c484af9226813151a373bc159
Kyle Desktop <kylespresser@gmail.com>
2018-11-16 15:18:58 -0500

    lecture 105

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -16,36 +16,49 @@
 // console.log(name6)
 
 // es5
-function driversLicense5(passedTest) {
-  if (passedTest) {
-    var firstName = 'John';
-    var yearOfBirth = 1990;
+// function driversLicense5(passedTest) {
+//   if (passedTest) {
+//     var firstName = 'John';
+//     var yearOfBirth = 1990;
 
-    console.log(
-      firstName + ' born in ' + yearOfBirth + ', is now allowed to drive.'
-    );
-  }
+//     console.log(
+//       firstName + ' born in ' + yearOfBirth + ', is now allowed to drive.'
+//     );
+//   }
+// }
+
+// driversLicense5(true);
+
+// // es6
+// function driversLicense6(passedTest) {
+//   let firstName;
+//   const yearOfBirth = 1990;
+//   if (passedTest) {
+//     firstName = 'John';
+//   }
+//   console.log(
+//     firstName + ' born in ' + yearOfBirth + ', is now allowed to drive.'
+//   );
+// }
+
+// driversLicense6(true);
+
+// var i = 23;
+
+// for (var i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+// console.log(i);
+
+// *********************************************************
+//                   BLOCKS AND IIFEs
+// *********************************************************
+
+{
+  const a = 1;
+  let b = 2;
+  var c = 3;
 }
 
-driversLicense5(true);
-
-// es6
-function driversLicense6(passedTest) {
-  let firstName;
-  const yearOfBirth = 1990;
-  if (passedTest) {
-    firstName = 'John';
-  }
-  console.log(
-    firstName + ' born in ' + yearOfBirth + ', is now allowed to drive.'
-  );
-}
-
-driversLicense6(true);
-
-var i = 23;
-
-for (var i = 0; i < 5; i++) {
-  console.log(i);
-}
-console.log(i);
+// console.log(a + b);
+console.log(c);
