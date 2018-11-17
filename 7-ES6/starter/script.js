5a0302afd1dc58bcdbbc720774b01a442f9fe8f3
Kyle Desktop <kylespresser@gmail.com>
2018-11-17 08:44:34 -0500

    lecture 107

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -67,21 +67,49 @@
 //                         STRINGS
 // *********************************************************
 
-let firstName = 'John';
-let lastName = 'Smith';
-const yearOfBirth = 1990;
-function calcAge(year) {
-  return 2018 - year;
-}
+// let firstName = 'John';
+// let lastName = 'Smith';
+// const yearOfBirth = 1990;
+// function calcAge(year) {
+//   return 2018 - year;
+// }
 
-console.log(
-  `This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today he is ${calcAge(
-    yearOfBirth
-  )} years old.`
-);
+// console.log(
+//   `This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today he is ${calcAge(
+//     yearOfBirth
+//   )} years old.`
+// );
 
-const n = `${firstName} ${lastName}`;
-console.log(n.startsWith('J'));
-console.log(n.endsWith('ith'));
-console.log(n.includes('n Sm'));
-console.log(firstName.repeat(5000));
+// const n = `${firstName} ${lastName}`;
+// console.log(n.startsWith('J'));
+// console.log(n.endsWith('ith'));
+// console.log(n.includes('n Sm'));
+// console.log(firstName.repeat(5));
+
+// *********************************************************
+//                     ARROW FUNCTIONS
+// *********************************************************
+
+const years = [1990, 1956, 1982, 1937];
+
+// es5
+var ages5 = years.map(function(el) {
+  return 2018 - el;
+});
+
+console.log(ages5);
+
+// es6
+let ages6 = years.map(el => 2018 - el);
+console.log(ages6);
+
+ages6 = years.map((el, i) => `Age element ${i + 1}: ${2018 - el}.`);
+console.log(ages6);
+
+ages6 = years.map((el, i) => {
+  const now = new Date().getFullYear();
+  const age = now - el;
+  return `Age element ${i + 1}: ${age}.`;
+});
+
+console.log(ages6);
