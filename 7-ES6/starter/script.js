99bb1a9399c332cae767ed9f565dd0ddf00bd158
Kyle Desktop <kylespresser@gmail.com>
2018-11-20 09:59:16 -0500

    lecture 108

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -90,26 +90,96 @@
 //                     ARROW FUNCTIONS
 // *********************************************************
 
-const years = [1990, 1956, 1982, 1937];
+// const years = [1990, 1956, 1982, 1937];
+
+// // es5
+// var ages5 = years.map(function(el) {
+//   return 2018 - el;
+// });
+
+// console.log(ages5);
+
+// // es6
+// let ages6 = years.map(el => 2018 - el);
+// console.log(ages6);
+
+// ages6 = years.map((el, i) => `Age element ${i + 1}: ${2018 - el}.`);
+// console.log(ages6);
+
+// ages6 = years.map((el, i) => {
+//   const now = new Date().getFullYear();
+//   const age = now - el;
+//   return `Age element ${i + 1}: ${age}.`;
+// });
+
+// console.log(ages6);
 
 // es5
-var ages5 = years.map(function(el) {
-  return 2018 - el;
-});
+var box5 = {
+  color: 'green',
+  position: 1,
+  clickMe: function() {
+    var self = this;
+    document.querySelector('.green').addEventListener('click', function() {
+      var str =
+        'This is box number ' + self.position + ' and it is ' + self.color;
+      alert(str);
+    });
+  }
+};
 
-console.log(ages5);
+// box5.clickMe();
 
 // es6
-let ages6 = years.map(el => 2018 - el);
-console.log(ages6);
+const box6 = {
+  color: 'green',
+  position: 1,
+  clickMe: function() {
+    document.querySelector('.green').addEventListener('click', () => {
+      var str =
+        'This is box number ' + this.position + ' and it is ' + this.color;
+      alert(str);
+    });
+  }
+};
 
-ages6 = years.map((el, i) => `Age element ${i + 1}: ${2018 - el}.`);
-console.log(ages6);
+box6.clickMe();
+// const box66 = {
+//   color: 'green',
+//   position: 1,
+//   clickMe: () => {
+//     document.querySelector('.green').addEventListener('click', () => {
+//       var str =
+//         'This is box number ' + this.position + ' and it is ' + this.color;
+//       alert(str);
+//     });
+//   }
+// };
 
-ages6 = years.map((el, i) => {
-  const now = new Date().getFullYear();
-  const age = now - el;
-  return `Age element ${i + 1}: ${age}.`;
-});
+// box66.clickMe();
 
-console.log(ages6);
+function Person(name) {
+  this.name = name;
+}
+
+// es5
+Person.prototype.myFriends5 = function(friends) {
+  var arr = friends.map(
+    function(el) {
+      return this.name + ' is friends with ' + el;
+    }.bind(this)
+  );
+  console.log(arr);
+};
+
+var friends = ['bob', 'jane', 'mark'];
+
+new Person('John').myFriends5(friends);
+
+// es6
+Person.prototype.myFriends6 = function(friends) {
+  const arr = friends.map(el => `${this.name} is friends with ${el}`);
+  console.log(arr);
+};
+
+new Person('Bill').myFriends6(friends);
