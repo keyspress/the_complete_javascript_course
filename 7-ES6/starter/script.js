52d51fed07a961a7ecda013696722a4f2951d8a5
Kyle Desktop <kylespresser@gmail.com>
2018-11-20 13:15:49 -0500

    lecture 115

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -369,38 +369,76 @@ function Person(name) {
 //                     MAPS
 // *********************************************************
 
-const question = new Map();
-question.set(
-  'question',
-  'What is the official name of the latest major JavaScript version?'
-);
-question.set(1, 'ES5');
-question.set(2, 'ES6');
-question.set(3, 'ES2015');
-question.set(4, 'ES7');
-question.set('correct', 3);
-question.set(true, 'Correct answer');
-question.set(false, 'Wrong answer');
-
-console.log(question.get('question'));
-// console.log(question.size);
-
-if (question.has(4)) {
-  // question.delete(4);
-  // console.log('it is here');
-}
-
-// question.clear();
-// question.forEach((value, key) =>
-//   console.log(`This is ${key}, and it's set to ${value}`)
+// const question = new Map();
+// question.set(
+//   'question',
+//   'What is the official name of the latest major JavaScript version?'
 // );
+// question.set(1, 'ES5');
+// question.set(2, 'ES6');
+// question.set(3, 'ES2015');
+// question.set(4, 'ES7');
+// question.set('correct', 3);
+// question.set(true, 'Correct answer');
+// question.set(false, 'Wrong answer');
 
-for (let [key, value] of question.entries()) {
-  if (typeof key === 'number') {
-    console.log(`Answer ${key}: ${value}`);
+// console.log(question.get('question'));
+// // console.log(question.size);
+
+// if (question.has(4)) {
+//   // question.delete(4);
+//   // console.log('it is here');
+// }
+
+// // question.clear();
+// // question.forEach((value, key) =>
+// //   console.log(`This is ${key}, and it's set to ${value}`)
+// // );
+
+// for (let [key, value] of question.entries()) {
+//   if (typeof key === 'number') {
+//     console.log(`Answer ${key}: ${value}`);
+//   }
+// }
+
+// const ans = parseInt(prompt('Write the correct answer'));
+
+// console.log(question.get(ans === question.get('correct')));
+
+// *********************************************************
+//                     CLASSES
+// *********************************************************
+
+// es5
+var Person5 = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+};
+
+Person5.prototype.calculateAge = function() {
+  var age = new Date().getFullYear - this.yearOfBirth;
+  console.log(age);
+};
+
+var john5 = new Person5('John', 1990, 'teacher');
+
+// es6
+class Person6 {
+  constructor(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+  }
+  calculateAge() {
+    const age = new Date().getFullYear - this.yearOfBirth;
+    console.log(age);
+  }
+  static greeting() {
+    console.log('Hey');
   }
 }
 
-const ans = parseInt(prompt('Write the correct answer'));
+const john6 = new Person6('John', 1990, 'teacher');
 
-console.log(question.get(ans === question.get('correct')));
+Person6.greeting();
