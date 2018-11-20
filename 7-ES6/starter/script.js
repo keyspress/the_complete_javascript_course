7cabd83a02affcdec9e7156d6308b9f7f01669e1
Kyle Desktop <kylespresser@gmail.com>
2018-11-20 12:33:39 -0500

    lecture 114

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -350,17 +350,57 @@ function Person(name) {
 // }
 
 // es6
-function SmithPerson(
-  firstName,
-  yearOfBirth,
-  lastName = 'Smith',
-  nationality = 'american'
-) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.yearOfBirth = yearOfBirth;
-  this.nationality = nationality;
+// function SmithPerson(
+//   firstName,
+//   yearOfBirth,
+//   lastName = 'Smith',
+//   nationality = 'american'
+// ) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.yearOfBirth = yearOfBirth;
+//   this.nationality = nationality;
+// }
+
+// var john = new SmithPerson('John', 1990);
+// var emily = new SmithPerson('Emily', 1983, 'Diaz', 'spanish');
+
+// *********************************************************
+//                     MAPS
+// *********************************************************
+
+const question = new Map();
+question.set(
+  'question',
+  'What is the official name of the latest major JavaScript version?'
+);
+question.set(1, 'ES5');
+question.set(2, 'ES6');
+question.set(3, 'ES2015');
+question.set(4, 'ES7');
+question.set('correct', 3);
+question.set(true, 'Correct answer');
+question.set(false, 'Wrong answer');
+
+console.log(question.get('question'));
+// console.log(question.size);
+
+if (question.has(4)) {
+  // question.delete(4);
+  // console.log('it is here');
 }
 
-var john = new SmithPerson('John', 1990);
-var emily = new SmithPerson('Emily', 1983, 'Diaz', 'spanish');
+// question.clear();
+// question.forEach((value, key) =>
+//   console.log(`This is ${key}, and it's set to ${value}`)
+// );
+
+for (let [key, value] of question.entries()) {
+  if (typeof key === 'number') {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
+
+const ans = parseInt(prompt('Write the correct answer'));
+
+console.log(question.get(ans === question.get('correct')));
