42281251c930642b5be3f823a0316715cee573aa
Kyle Desktop <kylespresser@gmail.com>
2018-11-23 16:25:03 -0500

    lecture 141

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,23 +1,28 @@
 import axios from 'axios';
 
-import Search from './models/Search';
-
 import { food2ForkKey } from '../keys';
+import { elements } from './views/base';
+import Search from './models/Search';
+import * as searchView from './views/searchView';
 
 // Global state
 const state = {};
 
 const controlSearch = async () => {
-  const query = 'pizza'; // todo
+  const query = searchView.getInput();
   if (query) {
     state.search = new Search(query);
+
+    searchView.clearInput();
+    searchView.clearResults();
+
     await state.search.getResults();
 
-    console.log(state.search.result);
+    searchView.renderResults(state.search.result);
   }
 };
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
   e.preventDefault();
   controlSearch();
 });
