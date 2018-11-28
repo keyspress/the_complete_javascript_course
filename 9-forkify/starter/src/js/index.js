e310f497eadf9281b632c24454bb24d8d5b83ffd
Kyle Desktop <kylespresser@gmail.com>
2018-11-28 13:52:27 -0500

    lecture 151

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -4,6 +4,7 @@ import { food2ForkKey } from '../keys';
 import { elements, renderLoader, clearLoader } from './views/base';
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 
@@ -95,5 +96,6 @@ elements.recipe.addEventListener('click', e => {
     state.recipe.updateServings('inc');
     recipeView.updateServingsIngredients(state.recipe);
   }
-  console.log(state.recipe);
 });
+
+window.l = new List();
