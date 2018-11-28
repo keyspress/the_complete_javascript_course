8fd0c04bd91f9b79a214c3b7569ab654c9c33228
Kyle Desktop <kylespresser@gmail.com>
2018-11-28 09:10:41 -0500

    lecture 148

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -5,6 +5,7 @@ import { elements, renderLoader, clearLoader } from './views/base';
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 
 // Global state
 const state = {};
@@ -57,7 +58,8 @@ const controlRecipe = async () => {
   console.log(id);
   if (id) {
     // prep ui for changes
-
+    recipeView.clearRecipe();
+    renderLoader(elements.recipe);
     // create new recipe object
     state.recipe = new Recipe(id);
 
@@ -69,7 +71,8 @@ const controlRecipe = async () => {
       state.recipe.calcTime();
       state.recipe.calcServings();
       // render recipe
-      console.log(state.recipe);
+      clearLoader();
+      recipeView.renderRecipe(state.recipe);
     } catch (err) {
       alert('Error processing recipe');
     }
--- 9-forkify/starter/src/js/models/Recipe.js
+++ 9-forkify/starter/src/js/models/Recipe.js
@@ -55,6 +55,7 @@ export default class Recipe {
       'cup',
       'pound'
     ];
+    const units = [...unitsShort, 'kg', 'g'];
     const newIngredients = this.ingredients.map(el => {
       // uniform units
       let ingredient = el.toLowerCase();
@@ -66,7 +67,7 @@ export default class Recipe {
 
       // parse ingredients into count, unit and ingredient
       const arrIng = ingredient.split(' ');
-      const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2));
+      const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
 
       let objIng;
       if (unitIndex > -1) {
